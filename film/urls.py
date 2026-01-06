1cfe4d0a5488546092ea3c9e77616710904795db
eltnpay <elyorkarimbaev55@gmail.com>
2026-01-06 15:46:25 +0500

    finish 1

--- film/urls.py
+++ film/urls.py
@@ -1,15 +1,15 @@
-
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls.static import static
 from django.conf import settings
-
-
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('movie.urls')),
+    path('', include('movie.urls')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL,document_root=settings.STATICFILES_DIRS)
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
